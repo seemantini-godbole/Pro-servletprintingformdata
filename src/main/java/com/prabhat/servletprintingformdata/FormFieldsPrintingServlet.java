c716537b03ae0a3a576185d5e7ae078572faadfe
seemantini-godbole <seemantini.godbole@gmail.com>
2015-06-08 07:23:04 +0530

    first commit for printing servlet

--- src/main/java/com/prabhat/servletprintingformdata/FormFieldsPrintingServlet.java
+++ src/main/java/com/prabhat/servletprintingformdata/FormFieldsPrintingServlet.java
@@ -1,4 +1,4 @@
-package com.prabhat.examplebasicservlet;
+package com.prabhat.servletprintingformdata;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -7,7 +7,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class HelloWorldServlet extends HttpServlet {
+public class FormFieldsPrintingServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
@@ -17,13 +17,19 @@ public class HelloWorldServlet extends HttpServlet {
 		out.println("<html>");
 
 		out.println("<head>");
-		out.println("<title>Hello Servlet</title>");
+		out.println("<title>FormFieldPrintingServlet</title>");
 		out.println("</head>");
 		
 		out.println("<body>");
-		out.println("<h3>Hello World! We are finally here after maven</h3>");
-		out.println("<h3>This file is checked into git</h3>");
+		out.println("<h3>This servlet prints all form data</h3>");
+		out.println("<h3>This file is checked into git and compiled with maven</h3>");
 		out.println("Date here is: " + (new Date()).toLocaleString());
+		
+		out.println("<p/>");
+		out.println("First Name is: " + req.getParameter("first_name"));
+		out.println("<br/>");
+		out.println("Last Name is: " + req.getParameter("last_name"));
+		
 		out.println("</body>");
 
 		out.println("</html>");
