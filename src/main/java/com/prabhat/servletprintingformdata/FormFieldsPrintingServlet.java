3f10c4e98295890a8881349af3eb763c3fb122a1
ria-vinod <ria.vinod99@gmail.com>
2015-06-09 06:10:24 +0530

    first change by ria-vinod

--- src/main/java/com/prabhat/servletprintingformdata/FormFieldsPrintingServlet.java
+++ src/main/java/com/prabhat/servletprintingformdata/FormFieldsPrintingServlet.java
@@ -31,6 +31,8 @@ public class FormFieldsPrintingServlet extends HttpServlet {
 		out.println("Last Name is: " + req.getParameter("last_name"));
 		
 		out.println("This is the line added after second commit in git by same local user");
+
+		out.println("This line was added by the contributor ri-vinod and merged into master");
 		
 		out.println("</body>");
 
